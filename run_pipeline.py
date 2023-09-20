9d0fa02c9ebb31d04f273d9a0f24cd4c4eca8b30
A158-debug <kumar.240@iitj.ac.in>
2023-09-20 19:24:56 +0530

    add production pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,7 +1,7 @@
 from pipelines.training_pipeline import train_pipeline
 
-from zenml.client import Client
+# from zenml.client import Client
 if __name__ == '__main__':
     # Run the pipeline
-    print(Client.activate_stack.experiment_tracker.get_tracking_uri())
+    # print(Client.activate_stack.experiment_tracker.get_tracking_uri())
     train_pipeline("D:\Projects\MLOps\Customer-Satisfaction-MLOps\data\olist_customers_dataset.csv")
