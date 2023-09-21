0c5f9ffcac42f3f3f7d988d13ef17e86b0a958f8
A158-debug <kumar.240@iitj.ac.in>
2023-09-21 19:21:27 +0530

    add streamlit app for production

--- pipelines/deployment_pipeline.py
+++ pipelines/deployment_pipeline.py
@@ -1,32 +1,29 @@
 import json
 
-# from .utils import get_data_for_test
-import os
 
+import os
 import numpy as np
 import pandas as pd
-from materializer.custom_materializer import cs_materializer
-from steps.clean_data import clean_data
-from steps.evaluation import evaluation
-from steps.ingest_data import ingest_data
+# from materializer.custom_materializer import cs_materializer
+from steps.clean_data import clean_df
+from steps.evaluation import evaluate_model
+from steps.ingest_data import ingest_df
 from steps.model_train import train_model
+
 from zenml import pipeline, step
 from zenml.config import DockerSettings
 from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
-from zenml.integrations.constants import MLFLOW, TENSORFLOW
+from zenml.integrations.constants import MLFLOW
 from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import (
     MLFlowModelDeployer,
 )
 from zenml.integrations.mlflow.services import MLFlowDeploymentService
 from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
-from zenml.steps import BaseParameters, Output
+from zenml.steps import BaseParameters
 
 from .utils import get_data_for_test
 
 docker_settings = DockerSettings(required_integrations=[MLFLOW])
-import pandas as pd
-
-# import os
 
 
 # from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import (
@@ -192,10 +189,10 @@ def continuous_deployment_pipeline(
     timeout: int = DEFAULT_SERVICE_START_STOP_TIMEOUT,
 ):
     # Link all the steps artifacts together
-    df = ingest_data()
-    x_train, x_test, y_train, y_test = clean_data(df)
+    df = ingest_df()
+    x_train, x_test, y_train, y_test = clean_df(df)
     model = train_model(x_train, x_test, y_train, y_test)
-    mse, rmse = evaluation(model, x_test, y_test)
+    mse, rmse = evaluate_model(model, x_test, y_test)
     deployment_decision = deployment_trigger(accuracy=mse)
     mlflow_model_deployer_step(
         model=model,
